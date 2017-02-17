f4f613e09710a0e5d8054e058b13056c14d4b472
Ben Boyter <ben@boyter.org>
2017-02-17 13:49:18 +1100

    Additional tests

--- vectorspace_test.go
+++ vectorspace_test.go
@@ -9,22 +9,22 @@ func TestConcordance(t *testing.T) {
 
 	got := concordance["this"]
 	if got != 1 {
-		t.Errorf("Cocordance expect (test) to have 1")
+		t.Errorf("Cocordance expect (test) to have 1 but got %q", got)
 	}
 
 	got = concordance["is"]
 	if got != 1 {
-		t.Errorf("Cocordance expect (test) to have 1")
+		t.Errorf("Cocordance expect (test) to have 1 but got %q", got)
 	}
 
 	got = concordance["a"]
 	if got != 1 {
-		t.Errorf("Cocordance expect (test) to have 1")
+		t.Errorf("Cocordance expect (test) to have 1 but got %q", got)
 	}
 
 	got = concordance["test"]
 	if got != 1 {
-		t.Errorf("Cocordance expect (test) to have 1")
+		t.Errorf("Cocordance expect (test) to have 1 but got %q", got)
 	}
 }
 
@@ -33,7 +33,51 @@ func TestMagnitude(t *testing.T) {
 	var got = Magnitude(concordance)
 
 	if got != 2 {
-		t.Errorf("Magnitude expected 2")
+		t.Errorf("Magnitude expected 2 but got %q", got)
 	}
 
+}
+
+func TestRelationSameExpectOne(t *testing.T) {
+	var concordance1 = Concordance("this is a test")
+	var concordance2 = Concordance("this is a test")
+
+	got := Relation(concordance1, concordance2)
+
+	if got != 1 {
+		t.Errorf("Relation expected 1 but got %q", got)
+	}
+}
+
+func TestRelationSimilarExpectSimilar(t *testing.T) {
+	var concordance1 = Concordance("this is a test")
+	var concordance2 = Concordance("this test")
+
+	got := Relation(concordance1, concordance2)
+
+	if got != 0.7071067811865475 {
+		t.Errorf("Relation expected 0.7071067811865475 but got %q", got)
+	}
+}
+
+func TestRelationDifferentExpectZero(t *testing.T) {
+	var concordance1 = Concordance("this is a test")
+	var concordance2 = Concordance("not related at all")
+
+	got := Relation(concordance1, concordance2)
+
+	if got != 0 {
+		t.Errorf("Relation expected 0.5 but got %q", got)
+	}
+}
+
+func TestRelationSimilarStrings(t *testing.T) {
+	var concordance1 = Concordance("Go has a lightweight test framework composed of the go test command and the testing package.")
+	var concordance2 = Concordance("Package testing provides support for automated testing of Go packages. It is intended to be used in concert with the go test command, which automates execution of any function of the form.")
+
+	got := Relation(concordance1, concordance2)
+
+	if got != 0.48211825989991874 {
+		t.Errorf("Relation expected 0.48211825989991874 but got %q", got)
+	}
 }
